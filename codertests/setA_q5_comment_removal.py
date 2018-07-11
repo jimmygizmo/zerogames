c79e56d5f9af40dc97f3ab89360d462a34c147f5
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2018-07-11 14:18:05 -0700

    setA q5 almost working. debugging it.

--- codertests/setA_q5_comment_removal.py
+++ codertests/setA_q5_comment_removal.py
@@ -21,18 +21,48 @@
 # { "_comment": "This is a comment in valid JSON, included as a valid data element." }
 # The implication is that such _comment data would need to be ignored by anything consuming the JSON.
 # So one can see a potential need for such a solution as this question # 5.
+#
+# NOTE: A corner case occurs when there are multiple URLs in a line.
+#
 
-json_data = """
-// this is a comment
+json_data = """// this is a comment
 { // another comment
 true, "foo", // 3rd comment
 "http://www.ariba.com" // comment after URL
 }
 """
 
-for line in json_data:
-    
+json_data_lines = json_data.splitlines()
+output = []
 
+print
+
+def process_line(line, comment_index):
+    #print "SUBSTRING: " + line[index-1:comment_index] + "\n"
+    if line[comment_index-1:comment_index] == ':':
+        print "URL SEEN: " + line + "\n"
+        print "URL CLUE BEFORE //: " + line[comment_index-1:comment_index] + "\n"
+        print "CONTINUING THROUGH REMAINDER OF THIS LINE"
+        line_remainder = line[comment_index+2:len(line)]
+        print "LINE REMAINDER: " + line_remainder + "\n"
+        find_comment_in_line_or_remainder(line_remainder, remainder_index)
+    else:
+        print "COMMENT PRESENT: " + line + "\n"
+
+def find_comment_in_line_or_remainder(line_or_remainder, remainder_index):
+    comment_index = line.find('//')
+    return comment_index
+
+
+for line in json_data_lines:
+    remainder_index = 0  # Always the case when we begin processing a line
+    comment_index = find_comment_in_line_or_remainder(line, remainder_index)
+    if comment_index == -1:
+        print "NO COMMENT: " + line + "\n"
+        output.append(line)
+        continue
+    else:
+        process_line(line, comment_index)
 
 ##
 #
