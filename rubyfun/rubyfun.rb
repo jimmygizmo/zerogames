480343663b2cf29db8e1a7e6fc4995f796c0694d
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2018-11-23 21:01:49 -0800

    commit via vscode

--- rubyfun/rubyfun.rb
+++ rubyfun/rubyfun.rb
@@ -1,4 +1,29 @@
 #! /usr/bin/env ruby
 
+puts "- - - - - - - - - - - - - - - -"
+
+num = 1
+
+while num <= 10
+    puts num
+    num += 1
+end
+
+puts "- - - - - - - - - - - - - - - -"
+
+puts "Hello. I am a ruby program. Please enter a number to square: "
+number = gets.chomp.to_f
+
+def square(number)
+    squared = number * number
+    return squared.to_s
+end
+
+puts "Your number is: " + number.to_s
+puts "Your number squared is: " + square(number)
+
+puts "- - - - - - - - - - - - - - - -"
+
+
 ##
 #
