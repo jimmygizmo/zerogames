13eaeca2af050f93c5daff7277d672d8ac1b339d
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2018-11-28 14:04:29 -0800

    ruby webrick example

---
+++ rubyfun/webrick.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/env ruby
+
+require 'webrick'
+
+root = File.expand_path '~/BENCH'
+server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+
+trap 'INT' do server.shutdown end
+
+server.start
+
+##
+#
